6b8ed3caff13a4f1a09b9825042e885cc8996420
Bat-sheva Birenbaum <b0556772946@gmail.com>
2025-03-11 14:37:23 +0200

    change docker

--- server/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bdb6b6e180e28824bc6945f39f1674ca1ca60fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b8b5d1b55fc6d63a4911f550541e57e8ceca9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
