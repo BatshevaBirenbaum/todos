205785dd18714d76645ce9a0f4560b8cba4b2ca0
Bat-sheva Birenbaum <b0556772946@gmail.com>
2025-03-18 15:02:20 +0200

    change port

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <section className="todoapp" >
       <header className="header">
-        <h1>משימות: 👌😏</h1>
+        <h1>  👌:משימות</h1>
         <form onSubmit={createTodo}>
           <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
         </form>
