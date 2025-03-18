f0a79fc6d9ba6515ebaae252818d5248b149534c
Bat-sheva Birenbaum <b0556772946@gmail.com>
2025-03-18 13:54:58 +0200

    good!

--- server/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc664f05644a5d1d7093e290e5652534ab737475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84840760d8deac52627b85029fba727f6941309d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
